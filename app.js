f677500e8ce35acb12eef56ff270f9cae76e9f8e
rdutta845 <rdutta845@gmail.com>
2017-02-03 15:26:23 +0530

    line 23 to 33

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 // For Prod usage (SECURITY)
 // app.use(helmet())
 
-mailer.extend(app, {
+/*mailer.extend(app, {
   from: config.email.from,
   host: config.email.host, // hostname
   secureConnection: true, // use SSL
@@ -30,7 +30,7 @@ mailer.extend(app, {
     pass: config.email.auth.pass
   }
 });
-
+*/
 mongoose.Promise = global.Promise;
 mongoose.connect(config.database);
 
