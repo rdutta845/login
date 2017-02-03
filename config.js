15f160b279c4c4032f6e1f6b669cf13f408d3eca
rdutta845 <rdutta845@gmail.com>
2017-02-03 15:24:13 +0530

    changed config.js

--- config.js
+++ config.js
@@ -4,26 +4,28 @@ module.exports = {
     //"database": "mongodb://localhost:27017/skeleton",
     "database": "mongodb://rdutta:9038084742@ds139949.mlab.com:39949/rdutta",
     "siteUrl": "http://localhost:3000",
-    "email": {
-      "from": "s26c.sayan@gmail.com",
-      "host": "smtp-pulse.com",
-      "port": 465,
-      "auth": {
-        "user": "s26c.sayan@gmail.com",
-        "pass": "Gne7SFoGLJ75B"
+//     "email": {
+//       "from": "s26c.sayan@gmail.com",
+//       "host": "smtp-pulse.com",
+//       "port": 465,
+//       "auth": {
+//         "user": "s26c.sayan@gmail.com",
+//         "pass": "Gne7SFoGLJ75B"
       },
+      
+      
    'production': {
     "secret": "bd17ddffc6694d25834a99ac2708a498",
     "database": "mongodb://rdutta:9038084742@ds139949.mlab.com:39949/rdutta",
     "siteUrl": "http://localhost:3000",
-    "email": {
-      "from": "s26c.sayan@gmail.com",
-      "host": "smtp-pulse.com",
-      "port": 465,
-      "auth": {
-        "user": "s26c.sayan@gmail.com",
-        "pass": "Gne7SFoGLJ75B"
-      }
-    }
+//     "email": {
+//       "from": "s26c.sayan@gmail.com",
+//       "host": "smtp-pulse.com",
+//       "port": 465,
+//       "auth": {
+//         "user": "s26c.sayan@gmail.com",
+//         "pass": "Gne7SFoGLJ75B"
+//       }
+//     }
   }
 };
