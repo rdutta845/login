1c14be65508edec67e4af59c9e0ac27896bf8652
rdutta845 <rdutta845@gmail.com>
2017-02-03 15:17:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,20 @@
 module.exports = {
   'development': {
     "secret": "bd17ddffc6694d25834a99ac2708a498",
-    "database": "mongodb://localhost:27017/skeleton",
+    //"database": "mongodb://localhost:27017/skeleton",
+    "database": "mongodb://rdutta:9038084742@ds139949.mlab.com:39949/rdutta",
+    "siteUrl": "http://localhost:3000",
+    "email": {
+      "from": "s26c.sayan@gmail.com",
+      "host": "smtp-pulse.com",
+      "port": 465,
+      "auth": {
+        "user": "s26c.sayan@gmail.com",
+        "pass": "Gne7SFoGLJ75B"
+      },
+   'production': {
+    "secret": "bd17ddffc6694d25834a99ac2708a498",
+    "database": "mongodb://rdutta:9038084742@ds139949.mlab.com:39949/rdutta",
     "siteUrl": "http://localhost:3000",
     "email": {
       "from": "s26c.sayan@gmail.com",
